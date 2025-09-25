2975fa806a445a15f1bbd32fc50000cb98375dd4
PirateChloe <piratechloe04@outlook.com>
2025-09-25 17:58:42 +0800

    banner海报swiper轮播器

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,53 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="homeLayout">
+		<!-- 轮播图 -->
+		<view class="banner">
+			<!-- indicator-dots是轮播图的指示器 -->
+			<!-- indicator-color指示器小圆点的颜色 -->
+			<!-- indicator-active-color指示器当前被选中的小圆点的颜色 -->
+			<!-- autoplay自动播放 -->
+			<!-- circular使用衔接滑动 -->
+			<swiper 
+			indicator-dots indicator-color="rgba(255,255,255,0.5)" 
+			indicator-active-color="#fff"
+			autoplay
+			circular
+			>
+				<swiper-item v-for="item in 3">
+					<image src="/common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss" scoped>
+.homeLayout{
+	.banner{
+		width: 750rpx;
+		padding: 30rpx 0;
+		
+		swiper{
+			// width: 750rpx;
+			width: 100%;
+			height: 340rpx;
+			
+			&-item{
+				width: 100%;
+				height: 100%;
+				padding: 0 30rpx;
+				
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+}
 </style>
