710536adfe616f7dc41f11eec2ed12a93c7ac1a7
PirateChloe <piratechloe04@outlook.com>
2025-09-29 15:41:43 +0800

    组件具名插槽定义公共标题模块

--- components/common-title/common-title.vue
+++ components/common-title/common-title.vue
@@ -1,20 +1,26 @@
 <template>
-	<view>
-		
+	<view class="common-title">
+		<view class="name">
+			<slot name="name"></slot>
+		</view>
+		<view class="custom">
+			<slot name="custom"></slot>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		name:"common-title",
-		data() {
-			return {
-				
-			};
-		}
-	}
+
 </script>
 
 <style>
-
+.common-title{
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 30rpx;
+	.name{
+		font-size: 40rpx;
+	}
+}
 </style>
